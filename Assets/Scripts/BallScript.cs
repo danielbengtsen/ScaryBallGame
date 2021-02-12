78e61841dcfb6845ea8f25c7b4cdcc868b964070
danielbengtsen <cph-db145@cphbusiness.dk>
2021-02-12 11:49:48 +0100

    Ball can roll - kind of

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * 100);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.back * 100);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.left * 100);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * 100);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
+        }
+    }
+}
